228bcff193288957364382b2ef769ce4eb9dac14
Viktoriya Lavrukhina <tomenko_viktoriya@mail.ru>
2023-03-20 16:33:01 +0300

    feat: style: code refactoring

--- js/catalog.js
+++ js/catalog.js
@@ -1,3 +1,206 @@
+//Добавляем элементы на страницу
+const jewelery = [
+  {
+    title : "Красивое кольцо",
+    image: "./images/content/catalog-items.png",
+    slider: [
+      "./images/content/image 107.png",
+      "./images/content/image 108.png",
+      "./images/content/image 107.png"
+    ],
+    stone: "Камень",
+    specification: "ст 11.97",
+    metal: "Белое золото",
+    size: "16.5",
+    note : "exclusive"
+  },
+  {
+    title : "Красивое кольцо",
+    image: "./images/content/catalog-items.png",
+    slider: [
+      "./images/content/image 107.png",
+      "./images/content/image 108.png",
+      "./images/content/image 107.png"
+    ],
+    stone: "Камень",
+    specification: "ст 11.97",
+    metal: "Белое золото",
+    size: "16.5",
+    note : "exclusive"
+  },
+  {
+    title : "Красивое кольцо",
+    image: "./images/content/catalog-items.png",
+    slider: [
+      "./images/content/image 107.png",
+      "./images/content/image 108.png",
+      "./images/content/image 107.png"
+    ],
+    stone: "Камень",
+    specification: "ст 11.97",
+    metal: "Белое золото",
+    size: "16.5",
+    note : "exclusive"
+  },
+  {
+    title : "Красивое кольцо",
+    image: "./images/content/catalog-items.png",
+    slider: [
+      "./images/content/image 107.png",
+      "./images/content/image 108.png",
+      "./images/content/image 107.png"
+    ],
+    stone: "Камень",
+    specification: "ст 11.97",
+    metal: "Белое золото",
+    size: "16.5",
+    note : "exclusive"
+  },
+  {
+    title : "Красивое кольцо",
+    image: "./images/content/catalog-items.png",
+    slider: [
+      "./images/content/image 107.png",
+      "./images/content/image 108.png",
+      "./images/content/image 107.png"
+    ],
+    stone: "Камень",
+    specification: "ст 11.97",
+    metal: "Белое золото",
+    size: "16.5",
+    note : "exclusive"
+  },
+  {
+    title : "Красивое кольцо",
+    image: "./images/content/catalog-items.png",
+    slider: [
+      "./images/content/image 107.png",
+      "./images/content/image 108.png",
+      "./images/content/image 107.png"
+    ],
+    stone: "Камень",
+    specification: "ст 11.97",
+    metal: "Белое золото",
+    size: "16.5",
+    note : "exclusive"
+  },
+  {
+    title : "Красивое кольцо",
+    image: "./images/content/catalog-items.png",
+    slider: [
+      "./images/content/image 107.png",
+      "./images/content/image 108.png",
+      "./images/content/image 107.png"
+    ],
+    stone: "Камень",
+    specification: "ст 11.97",
+    metal: "Белое золото",
+    size: "16.5",
+    note : "exclusive"
+  },
+  {
+    title : "Красивое кольцо",
+    image: "./images/content/catalog-items.png",
+    slider: [
+      "./images/content/image 107.png",
+      "./images/content/image 108.png",
+      "./images/content/image 107.png"
+    ],
+    stone: "Камень",
+    specification: "ст 11.97",
+    metal: "Белое золото",
+    size: "16.5",
+    note : "exclusive"
+  },
+];
+
+const jewelryTemplate = document.querySelector(".template-jewelry").content;
+const jewelryElements = document.querySelector(".catalog__elements");
+
+jewelery.forEach(function(element) {
+  const jewelryElement = jewelryTemplate.cloneNode(true);
+
+  jewelryElement.querySelector('.jewelry__image').src = element.image;
+  jewelryElement.querySelector('.jewelry__metal').textContent = element.metal;
+  jewelryElement.querySelector('.jewelry__size-value').textContent = element.size;
+  jewelryElement.querySelector('.jewelry__note').textContent = element.note;
+
+  jewelryElement.querySelectorAll('.jewelry__definition').forEach(el => {
+    el.textContent = element.title
+  });
+
+  jewelryElement.querySelectorAll('.jewelry__stone').forEach(stone => {
+    stone.textContent = element.stone
+  })
+  jewelryElement.querySelectorAll('.jewelry__specification').forEach(specification => {
+    specification.textContent = element.specification;
+  });
+
+  element.slider.forEach(item => {
+    const sliderItem = document.createElement('img');
+    sliderItem.src = item;
+    jewelryElement.querySelector('.jewelry-slider').append(sliderItem)
+  })
+
+  jewelryElements.append(jewelryElement)
+})
+
+//  Добавляем фильтры и мобильные фильтры на страницу 
+const filtersElements = [
+  {
+    title: "Камень",
+    options: ["Жёлтый бриллиант", "Бриллиант", "Изумруд", "Топаз", "Рубин", "Другие"]
+  },
+  {
+    title: "Огранка",
+    options: ["Круглая", "Груша", "Овал", "Маркиз", "Принцесса", "Античная"]
+  },
+  {
+    title: "Каратность",
+    options: ["1-2", "2-4", "6-8", "8 и более"]
+  },
+  {
+    title: "Металл",
+    options: ["Белое золото", "Розовое золото", "Серебро", "Платина"]
+  },
+];
+
+const filterTemplate = document.querySelector(".template-filter").content;
+const filterOptionTemplate = document.querySelector(".template-filter-option").content;
+const filterList = document.querySelector(".catalog__filters");
+
+filtersElements.forEach(function(element) {
+  const filterElement = filterTemplate.cloneNode(true);
+
+  filterElement.querySelector('.catalog__filter-title').textContent = element.title;
+
+  element.options.forEach(option => {
+    const filterOption = filterOptionTemplate.cloneNode(true);
+    filterOption.querySelector('.catalog__filter-option-value').textContent = option;
+    filterElement.querySelector('.catalog__filter-options').append(filterOption);
+  })
+
+  filterList.append(filterElement);
+});
+
+const mobileFilterTemplate = document.querySelector(".template-mobile-filters").content;
+const mobileFilterOptionTemplate = document.querySelector(".template-mobile-filter-option").content;
+const mobileFilterList = document.querySelector(".mobile-filters__list");
+
+filtersElements.forEach(function(element) {
+  const filterElement = mobileFilterTemplate.cloneNode(true);
+
+  filterElement.querySelector('.mobile-filters__filter-title-text').textContent = element.title;
+
+  element.options.forEach(option => {
+    const filterOption = mobileFilterOptionTemplate.cloneNode(true);
+    filterOption.querySelector('.catalog__filter-option-value').textContent = option;
+    filterElement.querySelector('.mobile-filters__submenu').append(filterOption);
+  })
+
+  mobileFilterList.append(filterElement);
+});
+
 // Открытие и закрытие массива из выпадающих фильтров
 let buttonsOpenFilter = document.querySelectorAll('.catalog__filter-button');
 
--- js/product.js
+++ js/product.js
@@ -58,3 +58,94 @@ document.querySelectorAll('.select').forEach(select => { //Выбриаем вс
     if (!select.contains(e.target))	selectListHide()
   })
 })
+
+// Добавляем рекомендованные украшения
+const recommendedProduct = [
+  {
+    title : "Красивое кольцо",
+    image: "./images/content/catalog-items.png",
+    slider: [
+      "./images/content/image 107.png",
+      "./images/content/image 108.png",
+      "./images/content/image 107.png"
+    ],
+    stone: "Камень",
+    specification: "ст 11.97",
+    metal: "Белое золото",
+    size: "16.5",
+    note : "exclusive"
+  },
+  {
+    title : "Красивое кольцо",
+    image: "./images/content/catalog-items.png",
+    slider: [
+      "./images/content/image 107.png",
+      "./images/content/image 108.png",
+      "./images/content/image 107.png"
+    ],
+    stone: "Камень",
+    specification: "ст 11.97",
+    metal: "Белое золото",
+    size: "16.5",
+    note : "exclusive"
+  },
+  {
+    title : "Красивое кольцо",
+    image: "./images/content/catalog-items.png",
+    slider: [
+      "./images/content/image 107.png",
+      "./images/content/image 108.png",
+      "./images/content/image 107.png"
+    ],
+    stone: "Камень",
+    specification: "ст 11.97",
+    metal: "Белое золото",
+    size: "16.5",
+    note : "exclusive"
+  },
+  {
+    title : "Красивое кольцо",
+    image: "./images/content/catalog-items.png",
+    slider: [
+      "./images/content/image 107.png",
+      "./images/content/image 108.png",
+      "./images/content/image 107.png"
+    ],
+    stone: "Камень",
+    specification: "ст 11.97",
+    metal: "Белое золото",
+    size: "16.5",
+    note : "exclusive"
+  },
+];
+
+const recommendedProductTemplate = document.querySelector('.template-recommended-product').content;
+const recommendedProductList = document.querySelector(".product-card__recommended-products-list");
+
+recommendedProduct.forEach(function(element) {
+	const productElement = recommendedProductTemplate.cloneNode(true);
+
+	productElement.querySelector('.jewelry__image').src = element.image;
+  productElement.querySelector('.jewelry__metal').textContent = element.metal;
+  productElement.querySelector('.jewelry__size-value').textContent = element.size;
+  productElement.querySelector('.jewelry__note').textContent = element.note;
+
+  productElement.querySelectorAll('.jewelry__definition').forEach(el => {
+    el.textContent = element.title
+  });
+
+  productElement.querySelectorAll('.jewelry__stone').forEach(stone => {
+    stone.textContent = element.stone
+  })
+  productElement.querySelectorAll('.jewelry__specification').forEach(specification => {
+    specification.textContent = element.specification;
+  });
+
+  element.slider.forEach(item => {
+    const sliderItem = document.createElement('img');
+    sliderItem.src = item;
+    productElement.querySelector('.jewelry-slider').append(sliderItem)
+  })
+
+  recommendedProductList.append(productElement)
+});
